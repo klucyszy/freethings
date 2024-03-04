95ac70aa1ab1f0858b342cd9b0290b93a02552bb
klucyszy <klucyszyn1995@gmail.com>
2024-03-04 21:54:41 +0100

    create structure folders

---
+++ source/backend/Freethings/Freethings/Application/DependencyInjection.cs
@@ -0,0 +1,9 @@
+namespace Freethings.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ source/backend/Freethings/Freethings/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,9 @@
+namespace Freethings.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+}
