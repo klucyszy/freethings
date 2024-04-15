5a91de35f1777ac67a40f01a3d81d73ccbeb3e37
klucyszy <klucyszyn1995@gmail.com>
2024-04-15 09:19:24 +0200

    Clean Offers folder

--- source/backend/Freethings/Freethings/Offers/Application/DependencyInjection.cs
+++
@@ -1,9 +0,0 @@
-namespace Freethings.Offers.Application;
-
-internal static class DependencyInjection
-{
-    public static IServiceCollection AddApplication(this IServiceCollection services)
-    {
-        return services;
-    }
-}
